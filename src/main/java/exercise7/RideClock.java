6d697476b440596e0ff7b0af48fa5643e5715a6a
Paige Ruppel <paige.ruppel@gmail.com>
2017-11-10 10:38:47 -0500

    wrote logic to pass first two test cases

--- src/main/java/exercise7/RideClock.java
+++ src/main/java/exercise7/RideClock.java
@@ -3,7 +3,14 @@ package exercise7;
 public class RideClock {
 
 	public int lateRide(int n) {
-		return 14;
+		int firstHour = n / 600;
+		n -= firstHour * 600;
+		int secondHour = n / 60;
+		n -= 60 * secondHour;
+		int firstMinute = n / 10;
+		n -= 10 * firstMinute;
+		int secondMinute = n;
+		return firstHour + secondHour + firstMinute + secondMinute;
 	}
 
 }

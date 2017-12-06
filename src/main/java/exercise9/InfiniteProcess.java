17ab7c8743f4402abbf6ecd5b105f793dc129c1d
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-06 16:28:09 -0500

    passed first two test cases

--- src/main/java/exercise9/InfiniteProcess.java
+++ src/main/java/exercise9/InfiniteProcess.java
@@ -3,7 +3,15 @@ package exercise9;
 public class InfiniteProcess {
 
 	public boolean isInfiniteProcess(int a, int b) {
-		return false;
+		while (a <= b) {
+			if (a == b) {
+				return false;
+			} else {
+				a++;
+				b--;
+			}
+		}
+		return true;
 	}
 
 }
--- src/test/java/exercise9/InfiniteProcessTest.java
+++ src/test/java/exercise9/InfiniteProcessTest.java
@@ -1,6 +1,7 @@
 package exercise9;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,5 +20,10 @@ public class InfiniteProcessTest {
 		int a = 2;
 		int b = 6;
 		assertFalse(underTest.isInfiniteProcess(a,b));
+	}
+	
+	@Test
+	public void shouldReturnTrueForA2AndB3() {
+		assertTrue(underTest.isInfiniteProcess(2, 3));
 	}
 }

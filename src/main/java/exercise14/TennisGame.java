612897b34fedc2e5b376058e1b93ff6bb55c1622
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-13 10:40:29 -0500

    passed second test case

--- src/main/java/exercise14/TennisGame.java
+++ src/main/java/exercise14/TennisGame.java
@@ -2,6 +2,7 @@ package exercise14;
 
 public class TennisGame {
     public boolean tennisSet(int score1, int score2) {
-        return true;
+        int max = (score1 < score2) ? score2 : score1;
+        return Math.abs(score1 - score2) >= 3 && max < 7;
     }
 }
--- src/test/java/exercise14/TennisGameTest.java
+++ src/test/java/exercise14/TennisGameTest.java
@@ -3,6 +3,7 @@ package exercise14;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TennisGameTest {
@@ -17,5 +18,10 @@ public class TennisGameTest {
     @Test
     public void tennisSetScore13AndScore26ShouldReturnTrue() {
         assertTrue(underTest.tennisSet(3, 6));
+    }
+
+    @Test
+    public void tennisSetScore18AndScore25ShouldReturnFalse() {
+        assertFalse(underTest.tennisSet(8,5));
     }
 }

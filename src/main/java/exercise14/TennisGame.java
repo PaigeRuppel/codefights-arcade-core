f42c44595949d320148fc621ea06cd6d6d35f02d
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-13 11:14:58 -0500

    setup first test for exercise 15

--- src/main/java/exercise14/TennisGame.java
+++ src/main/java/exercise14/TennisGame.java
@@ -2,12 +2,12 @@ package exercise14;
 
 public class TennisGame {
     public boolean tennisSet(int score1, int score2) {
+        int winningScoreMinBelow5 = 6;
+        int winningScoreMinAtOrAbove5 = 7;
+        int max = Math.max(score1, score2);
+        int min = Math.min(score1, score2);
         boolean tie = score1 == score2;
-        int winBelow5 = 6;
-        int winAtOrAbove5 = 7;
-        int max = (score1 < score2) ? score2 : score1;
-        int min = (score1 < score2) ? score1 : score2;
-        boolean matchRule = (min < 5) ? max == winBelow5 : max == winAtOrAbove5;
+        boolean matchRule = (min < 5) ? max == winningScoreMinBelow5 : max == winningScoreMinAtOrAbove5;
         return !tie && matchRule;
     }
 }
---
+++ src/main/java/exercise15/Love.java
@@ -0,0 +1,9 @@
+package exercise15;
+
+public class Love {
+
+
+    public boolean willYou(boolean young, boolean beautiful, boolean loved) {
+        return true;
+    }
+}

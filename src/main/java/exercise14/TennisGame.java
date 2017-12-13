cf86c8c8f21d5f0076b36a0a420b997aefc186d2
Paige Ruppel <paige.ruppel@gmail.com>
2017-12-13 10:55:45 -0500

    solution passed, refactored

--- src/main/java/exercise14/TennisGame.java
+++ src/main/java/exercise14/TennisGame.java
@@ -2,11 +2,12 @@ package exercise14;
 
 public class TennisGame {
     public boolean tennisSet(int score1, int score2) {
+        boolean tie = score1 == score2;
         int winBelow5 = 6;
         int winAtOrAbove5 = 7;
-        boolean tie = score1 == score2;
         int max = (score1 < score2) ? score2 : score1;
         int min = (score1 < score2) ? score1 : score2;
-        return !tie && ((min < 5 && max == winBelow5) || (min >= 5 && max == winAtOrAbove5));
+        boolean matchRule = (min < 5) ? max == winBelow5 : max == winAtOrAbove5;
+        return !tie && matchRule;
     }
 }
